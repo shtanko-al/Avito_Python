b157c6a6737081c964bce9032d70724b40d041d9
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-04-02 01:19:47 +0300

    Add files via upload

---
+++ home_work-3/issue-3/test_one_hot_encoder.py
@@ -0,0 +1,28 @@
+import unittest
+from one_hot_encoder import fit_transform
+
+
+class TestOneHotEncoder(unittest.TestCase):
+    def test_many_str(self):
+        result = fit_transform('pies', 'work', 'may')
+        self.assertEqual(result, [('pies', [0, 0, 1]), ('work', [0, 1, 0]), ('may', [1, 0, 0])])
+
+    def test_list(self):
+        result = fit_transform([1, '2', 3])
+        self.assertEqual(result, [(1, [0, 0, 1]), ('2', [0, 1, 0]), (3, [1, 0, 0])])
+
+    def test_str_with_double(self):
+        result = fit_transform('Paris', 'Ney-York', 'Paris')
+        self.assertNotEqual(result, [('Paris', [0, 0, 1]), ('Ney-York', [0, 1, 0]), ('Paris', [1, 0, 0])])
+
+    def test_4(self):
+        result = fit_transform('1', 'stone', '2', '1', 'stone')
+        expectation = [('1', [0, 0, 1]), ('stone', [0, 1, 0]), ('2', [1, 0, 0]), ('1', [0, 0, 1]), ('stone', [0, 1, 0])]
+        self.assertIn(result[0], expectation)
+
+    def test_5(self):
+        with self.assertRaises(TypeError):
+            fit_transform()
+
+if __name__ == '__main__':
+    unittest.main()
