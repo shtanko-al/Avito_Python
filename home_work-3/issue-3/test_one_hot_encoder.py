885b163589a89eae0c057321bbc407927b2245c2
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-04-02 03:43:23 +0300

    Update test_one_hot_encoder.py

--- home_work-3/issue-3/test_one_hot_encoder.py
+++ home_work-3/issue-3/test_one_hot_encoder.py
@@ -17,12 +17,13 @@ class TestOneHotEncoder(unittest.TestCase):
 
     def test_4(self):
         result = fit_transform('1', 'stone', '2', '1', 'stone')
-        expectation = [('1', [0, 0, 1]), ('stone', [0, 1, 0]), ('2', [1, 0, 0]), ('1', [0, 0, 1]), ('stone', [0, 1, 0])]
-        self.assertIn(result[0], expectation)
+        expect = [('1', [0, 0, 1]), ('stone', [0, 1, 0]), ('2', [1, 0, 0]), ('1', [0, 0, 1]), ('stone', [0, 1, 0])]
+        self.assertIn(result[0], expect)
 
     def test_5(self):
         with self.assertRaises(TypeError):
             fit_transform()
+
 
 if __name__ == '__main__':
     unittest.main()
