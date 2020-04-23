00dd8a387f600149cd97debe8febe92ffe3efd6d
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-04-23 10:17:14 +0300

    Update hw_4.py

--- home_work-4/hw_4.py
+++ home_work-4/hw_4.py
@@ -3,7 +3,8 @@ from copy import deepcopy
 
 
 def ilen(iterable: Iterable):
-    # 1) функция получения размера генератора
+    """1) функция возвращает размер итерируемого объекта
+    или генератора, если объект пустой, то None"""
     n = 0
     for _ in iterable:
         n += 1
@@ -11,7 +12,9 @@ def ilen(iterable: Iterable):
 
 
 def flatten(iterable: Iterable):
-    # 2) функцию flatten, которая из многоуровневого массива сделает одноуровневый
+    """
+    2) функция flatten, которая из многоуровневого массива сделает одноуровневый
+    """
     new_list = list()
     for i in iterable:
         if not isinstance(i, Iterable):
@@ -23,7 +26,7 @@ def flatten(iterable: Iterable):
 
 
 def distinct(iterable: Iterable):
-    # 3) функция, которая удалит дубликаты, сохранив порядок
+    """3) функция, которая удалит дубликаты, сохранив порядок"""
     temp_list = list()
     for i in iterable:
         if i not in temp_list:
@@ -32,8 +35,10 @@ def distinct(iterable: Iterable):
 
 
 def groupby(key, iterable: Iterable):
-    # 4) функция делает из неупорядоченной последовательностей словарей,
-    #    словарь сгруппированный по ключу
+    """
+    4) функция делает из неупорядоченной последовательностей словарей,
+    словарь сгруппированный по ключу
+    """
     result_dict = dict()
     for i in iterable:
         # print('value of i: ', i)
@@ -48,7 +53,7 @@ def groupby(key, iterable: Iterable):
 
 
 def chunks(size: int, iterable: Iterable):
-    # 5) функция, которая разобьет последовательность на заданные куски
+    """5) функцию, которая разобьет последовательность на заданные куски"""
     temp_list = list()
     result_list = list()
     n = 0
@@ -57,8 +62,6 @@ def chunks(size: int, iterable: Iterable):
         n += 1
         if n == size:
             temp_list_2 = deepcopy(temp_list)
-            # возможно использование deepcopy это моветон
-            # и надо более алгоритмически сделать
             result_list.append(temp_list_2)
             temp_list.clear()
             n = 0
@@ -68,70 +71,72 @@ def chunks(size: int, iterable: Iterable):
 
 
 def first(iterable: Iterable):
-    # 6) функция получения первого элемента или None
+    """6) функция получения первого элемента или None"""
+    my_iterator = iter(iterable)
     try:
-        return next(iter(iterable))
+        return next(my_iterator)
     except StopIteration:
         return None
 
 
-def last(iterable: Iterable):
-    #  7) функция получения последнего элемента или None
-    #     не работает с генераторами
-    if len(iterable) == 0:
-        return None
-    else:
-        return iterable[len(iterable)-1]
-    
-def last_2(iterable: Iterable):
-    #  7) функция получения последнего элемента или None
-    # теперь работает с генераторами, но, наверно, можно лучше сделать
-    my_iterator = iter(iterable)
-    n = 0
-    while True:
-        try:
-            temp = next(my_iterator)
-            # print(temp)
-            n += 1
-        except StopIteration:
-            if n == 0:
-                return None
-            else:
-                return temp
+# def last(iterable: Iterable):
+#     #  7) функция получения последнего элемента или None
+#     if len(iterable) == 0:
+#         return None
+#     else:
+#         return iterable[len(iterable)-1]
+#
+# def last_2(iterable: Iterable):
+#     #  7) функция получения последнего элемента или None
+#     # работает с генераторами, но, наверно, можно лучше сделать
+#     my_iterator = iter(iterable)
+#     n = 0
+#     while True:
+#         try:
+#             temp = next(my_iterator)
+#             # print(temp)
+#             n += 1
+#         except StopIteration:
+#             if n == 0:
+#                 return None
+#             else:
+#                 return temp
 
-def last_3(iterable: Iterable):
-    #  7) функция получения последнего элемента или None
+def last(iterable: Iterable):
+    """7) функция возвращает последний элемент
+    из итерируемого объекта или генератора
+    или None, если объект пустой"""
     i = None
     for i in iterable:
         pass
     return i
 
 
-############################################################
-# проверки:
-test_list_1 = [-1, 0, 7, 8, [1, [2, 3]], 11, 12]
-test_list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-test_list_3 = ['a', 'b', 'c', 'd', 'e', 'f']
-foo = (x for x in range(10))
-print(ilen(test_list_1))
-print(flatten(test_list_2))
-foo = (x for x in range(10))
-print('проверка функции chunks: ', chunks(3, test_list_1))
-print('проверка функции chunks: ', chunks(3, test_list_3))
-print()
-print('проверка функции first: ', first(foo))
-print('проверка функции first: ', first(test_list_3))
-print('проверка функции first: ', first(range(0)))
-print()
-print('проверка функции last: ', last_3(range(0)))
-print('проверка функции last: ', last_3(foo))
-print('проверка функции last: ', last_3(test_list_3))
+if __name__ == '__main__':
+    # проверки:
+    test_list_1 = [-1, 0, 7, 8, [1, [2, 3]], 11, 12]
+    test_list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    test_list_3 = ['a', 'b', 'c', 'd', 'e', 'f']
+    foo = (x for x in range(10))
+    print(ilen(test_list_1))
+    print(flatten(test_list_2))
+    foo = (x for x in range(10))
+    print('проверка функции chunks: ', chunks(3, test_list_1))
+    print('проверка функции chunks: ', chunks(3, test_list_3))
+    print()
+    print('проверка функции first: ', first(foo))
+    print('проверка функции first: ', first(test_list_3))
+    print('проверка функции first: ', first(range(0)))
+    print()
+    print('проверка функции last_3: ', last(range(0)))
+    print('проверка функции last_3: ', last(foo))
+    print('проверка функции last_3: ', last(test_list_3))
 
-users = [
+    users = [
         {'gender': 'female', 'age': 33},
         {'gender': 'male', 'age': 20},
         {'gender': 'female', 'age': 21},
-]
-print()
-print('функция groupby: ', groupby('gender', users))
-print('функция groupby: ', groupby('age', users))
+    ]
+    print()
+    print('функция groupby: ', groupby('gender', users))
+    print('функция groupby: ', groupby('age', users))
