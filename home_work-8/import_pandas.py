d7fe0ec326888ca5f79dd0f75fe2e48a71dc3b11
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-05-22 20:00:12 +0300

    Update import_pandas.py

--- home_work-8/import_pandas.py
+++ home_work-8/import_pandas.py
@@ -9,25 +9,27 @@ DIR = 'other_user_logs'
 def loader_csv(logs_path: str):
     """"
     функция загружает файлы csv из папки, возвращает объект Pandas
+    logs_path - имя папки с файлами для загрузки
     """
     csv_files_list = [f for f in os.listdir(logs_path) if f[-3:] == 'csv']  # выборка только csv
     if len(csv_files_list) == 0:
-        raise Exception('в папке {} не csv файлов'.format(logs_path))
-        
+        raise Exception('в папке {} нет csv файлов'.format(logs_path))
+
     file = open('{}./{}'.format(logs_path, csv_files_list[0]), 'r')
-    column_name = file.readline().rstrip().split(',')  # названия для второго и третьего столбца:
-    file.close()
-    
-    data_list = []  # список словарей с данными
+    column_name = file.readline().strip().split(',')  # названия для второго и третьего столбца:
+    file.close()                                      # для сравнения с остальными файлами
+
+    data_list = []  # список словарей с данными для DataFrame
     for file_name in csv_files_list:
         with open('{}./{}'.format(logs_path, file_name), 'r') as file_to_read:
-            new_column_name = file_to_read.readline().rstrip().split(',')
+            new_column_name = file_to_read.readline().strip().split(',')
             if new_column_name != column_name:  # проверка, что у всех файлов правильная структура заголовков
                 raise Exception('файл {} или {} с неправильной структурой'.format(file_name, csv_files_list[0]))
+            user_name = file_name[0:file_name.find('.')]  # на всякий случай, если имена у файлов с разной структурой
             for line in file_to_read:
-                line_data = line.rstrip().split(',')
+                line_data = line.strip().split(',')
                 temp_dict = {
-                    'user_id': file_name[0:-4],
+                    'user_id': user_name,
                     column_name[0]: line_data[0],
                     column_name[1]: line_data[1]
                 }
@@ -39,6 +41,6 @@ if __name__ == '__main__':
     startTime = time.perf_counter()
 
     super_frame = loader_csv('other_user_logs')
-    print('время работы скрипта 1', time.perf_counter() - startTime)
+    print('время работы скрипта 1: ', time.perf_counter() - startTime)
 
     print(super_frame.info())
