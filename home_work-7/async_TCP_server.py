b9ed2ec050cdba232778a489db1a3cda19556560
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-05-28 09:24:43 +0300

    Update async_TCP_server.py

--- home_work-7/async_TCP_server.py
+++ home_work-7/async_TCP_server.py
@@ -4,6 +4,7 @@ TCP сервер, асинхронный
 import asyncio
 import datetime
 
+
 class EchoServerClientProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
@@ -21,33 +22,33 @@ class EchoServerClientProtocol(asyncio.Protocol):
             print(date_f)
             print(type(date_f))
             self.transport.write(date_f.encode())
-        elif message[0:4] == 'exho':
+        elif message[0:4] == 'echo':
             poz = message.find(' ')
-            mes = message[poz+1:]
+            mes = message[poz + 1:]
             self.transport.write(mes.encode())
         else:
-            mes = """доступные комманды:
-* echo `<message>` – возвращает <message>
-* calendar – возвращает клиенту текущее время в формате dd.mm.YYYY HH:MM
-* stop – закрывает сервер
-* любая другая команда – вывод сообщения о доступных командах
-                """
+            mes = 'доступные комманды:\
+                * echo `<message>` – возвращает <message>\
+                * calendar – возвращает клиенту текущее время в формате dd.mm.YYYY HH:MM\
+                * stop – закрывает сервер\
+                * любая другая команда – вывод сообщения о доступных командах\
+                '
             self.transport.write(mes.encode())
 
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    # Each client connection will create a new protocol instance
+    coro = loop.create_server(EchoServerClientProtocol, '127.0.0.1', 10001)
+    server = loop.run_until_complete(coro)
 
-loop = asyncio.get_event_loop()
-# Each client connection will create a new protocol instance
-coro = loop.create_server(EchoServerClientProtocol, '127.0.0.1', 10001)
-server = loop.run_until_complete(coro)
+    # Serve requests until Ctrl+C is pressed
+    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        print('сервер должен работать')
 
-# Serve requests until Ctrl+C is pressed
-print('Serving on {}'.format(server.sockets[0].getsockname()))
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    print('сервер должен работать')
-
-# Close the server
-# server.close()
-# loop.run_until_complete(server.wait_closed())
-# loop.close()
+    # Close the server
+    # server.close()
+    # loop.run_until_complete(server.wait_closed())
+    # loop.close()
