86fa92196fd2d7fb1ece8943d62783c9c4c5fcc2
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-06-04 10:55:04 +0300

    Create test_berry.py

---
+++ exam/test_berry.py
@@ -0,0 +1,27 @@
+import pytest
+from ver_2 import berry_coun
+
+
+def test_1():
+    my_input = [0, 1]
+    expect = ImportError
+    assert berry_coun(my_input) == expect
+
+
+def test_2():
+    my_input = [1, 1, 1, 1, 1]
+    expect = 0
+    assert berry_coun(my_input) == expect
+
+def test_3():
+    my_input = [1, 1, 5, 1, 1]
+    expect = 1
+    assert berry_coun(my_input) == expect
+
+def test_4():
+    my_input = [1, 1, 5, 2, 1]
+    expect = 2
+    assert berry_coun(my_input) == expect
+
+if __name__ == '__main__':
+    pytest.main()
