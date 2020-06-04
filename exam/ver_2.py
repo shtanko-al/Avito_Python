d25905e08c0252e2cd000a7d1d7fc1e3148d0fc1
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-06-04 11:40:37 +0300

    Update ver_2.py

--- exam/ver_2.py
+++ exam/ver_2.py
@@ -1,28 +1,28 @@
-kusts = [2, 11, 7, 1, 8, 1]
-
-
 def berry_coun(k: list):
     if len(k) < 3:
-        return ImportError
-    kusts = k
-    kusts.insert(0, kusts[-1])
-    kusts.append(kusts[1])
-    max = kusts[-1] + kusts[0] + kusts[1]
+        raise ImportError
 
+    max = k[-1] + k[0] + k[1]
     poz = 0
-    for i in range(1, len(kusts) - 1):
-        temp_max = kusts[i - 1] + kusts[i] + kusts[i + 1]
+
+    for i in range(1, len(k) - 1):
+        temp_max = k[i - 1] + k[i] + k[i + 1]
         if temp_max > max:
             max = temp_max
-            poz = i - 1
+            poz = i
+
+    if k[-2] + k[-1] + k[0] > max:
+        poz = len(k) - 1
+
     return poz
 
 
 if __name__ == '__main__':
     k_list_1 = [0, 1, 2, 3, 4, 5, 6, 7]
     k_list_2 = [1, 2, 3]
-    k_list_3 = [0, 1]
-    
+    # k_list_3 = [0, 1]
+
     print(berry_coun(k_list_1))
     print(berry_coun(k_list_2))
-    print(berry_coun(k_list_3))
+    # print(berry_coun(k_list_3))
+    print(k_list_2)
