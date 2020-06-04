4f326e574c040908ee25934fdfed4271e9a07717
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-06-04 11:04:18 +0300

    Update main.py

--- exam/main.py
+++ exam/main.py
@@ -1,27 +1,29 @@
-from random import randint
+kusts = [2, 11, 7, 1, 8, 1]
 
-print('Введите количество кустов (не меньше 3)')
-size = input()
-while True:
-    if size.isdigit() and int(size) >= 3:
-        size = int(size)
-        break
-    else:
-        print('Введите число больше 3')
-        size = input()
 
-kusts = [randint(0, 51) for _ in range(size)]
-print(kusts)
+def berry_coun(k: list):
+    if len(k) < 3:
+        raise ImportError
 
-kusts.insert(0, kusts[-1])
-kusts.append(kusts[1])
-max = kusts[-1] + kusts[0] + kusts[1]
+    kusts = k
+    kusts.insert(0, kusts[-1])
+    kusts.append(kusts[1])
+    max = kusts[-1] + kusts[0] + kusts[1]
 
-poz = 0
-for i in range(1, size + 1):
-    temp_max = kusts[i - 1] + kusts[i] + kusts[i + 1]
-    if temp_max > max:
-        max = temp_max
-        poz = i - 1
+    poz = 0
+    for i in range(1, len(kusts) - 1):
+        temp_max = kusts[i - 1] + kusts[i] + kusts[i + 1]
+        if temp_max > max:
+            max = temp_max
+            poz = i - 1
+    return poz
 
-print('индекс нужного куста: ', poz)
+
+if __name__ == '__main__':
+    k_list_1 = [0, 1, 2, 3, 4, 5, 6, 7]
+    k_list_2 = [1, 2, 3]
+    k_list_3 = [0, 1]
+
+    print(berry_coun(k_list_1))
+    print(berry_coun(k_list_2))
+    print(berry_coun(k_list_3))
