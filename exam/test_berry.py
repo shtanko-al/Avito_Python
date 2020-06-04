67bc7af2598af20d6fa0d746029e937ebdeff96f
Aleksandr <55200888+shtanko-al@users.noreply.github.com>
2020-06-04 10:57:33 +0300

    Update test_berry.py

--- exam/test_berry.py
+++ exam/test_berry.py
@@ -13,15 +13,18 @@ def test_2():
     expect = 0
     assert berry_coun(my_input) == expect
 
+
 def test_3():
     my_input = [1, 1, 5, 1, 1]
     expect = 1
     assert berry_coun(my_input) == expect
 
+
 def test_4():
     my_input = [1, 1, 5, 2, 1]
     expect = 2
     assert berry_coun(my_input) == expect
+
 
 if __name__ == '__main__':
     pytest.main()
